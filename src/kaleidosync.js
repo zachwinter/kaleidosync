b49324c8223f5c5a468685c689f119fe238cf62f
Zach Winter <contact@zachwinter.com>
2019-01-14 15:00:25 -0800

    Removed remaining mutations from `Kaleidosync` class and into mutator functions.

--- src/kaleidosync.js
+++ src/kaleidosync.js
@@ -1,5 +1,5 @@
 import state from './state'
-import { initCanvas, sizeCanvas } from './util/canvas'
+import { initCanvas } from './util/canvas'
 import {
   initParameters, 
   initState,
@@ -9,27 +9,36 @@ import {
   setStarColor, 
   setBackgroundColor, 
   setActiveSize,
-  setActiveInterval,
+  setActiveIntervals,
   setInitialStart,
-  setTrackProgress
+  setTrackProgress,
+  resizeElements,
+  paint
 } from './state/mutations'
 import { ping } from './state/actions'
 
 export default class Kaleidosync {
   constructor () {
+    /** Initialize <canvas> and 2d context. */
     const { canvas, ctx } = initCanvas()
     this.canvas = canvas
     this.ctx = ctx
+    
+    /** Initialize reactive app state. */
     this.state = state
 
     /** Get API tokens from cookies. */
     setTokens(this.state)
+    
     /** Initialize visualizer parameters and initial state. */
     initParameters(this.state)
     initState(this.state, this.canvas)
 
+    /** Watch for changes in state. */
     this.watch()
-    this.onResize()
+    
+    /** Adjust visualizer on window resize. */
+    window.addEventListener('resize', () => resizeElements(this.state, this.canvas))
 
     /** Ping Spotify for currently playing track. */
     ping(this.state)
@@ -62,47 +71,12 @@ export default class Kaleidosync {
       setBackgroundColor(this.state, 'beats')
     })
 
-    /** On every fourth bar, set actvie color scheme. */
+    /** On every fourth bar, set active color scheme. */
     intervals.watch('bars', ({ index }) => {
       if (index % 4 === 0) {
         setColorScheme(this.state)
       }
     })
-  }
-
-  /**
-   * @method onResize – Adjust <canvas> and stars on window resize.
-   */
-  onResize () {
-    window.addEventListener('resize', () => {
-      initParameters(this.state)
-      sizeCanvas(this.canvas)
-
-      this.state.visualizer.stars.forEach(star => {
-        star.x = this.canvas.width / 2
-        star.y = this.canvas.height / 2
-      })
-    })
-  }
-
-  /**
-   * @method determineInterval – Determine and return active interval index of type, based on current track progress.
-   * @param type – Type of interval.
-   */
-  determineInterval(type) {
-    for (let i = 0; i < this.state.visualizer.trackAnalysis[type].length; i++) {
-      /** If last interval... */
-      if (i === (this.state.visualizer.trackAnalysis[type].length - 1)) {
-        return i
-      }
-
-      /** If current track progress falls within current interval. */
-      if (this.state.visualizer.trackAnalysis[type][i].start < this.state.visualizer.trackProgress && this.state.visualizer.trackProgress < this.state.visualizer.trackAnalysis[type][i + 1].start) {
-        return i
-      }
-    }
-
-    return -1
   }
 
   /**
@@ -130,32 +104,15 @@ export default class Kaleidosync {
     }
       
     /** progress = (now - initialStart) + initialTrackProgress */
-    setTrackProgress(state, (timestamp - this.state.visualizer.initialStart) + this.state.visualizer.initialTrackProgress)
+    setTrackProgress(this.state, (timestamp - this.state.visualizer.initialStart) + this.state.visualizer.initialTrackProgress)
 
-    /** For each interval type... */
-    this.state.visualizer.intervalTypes.forEach(type => {
-      const index = this.determineInterval(type)
+    /** Based on track progress, determine active intervals. */
+    setActiveIntervals(this.state)
 
-      /** If determined index is not equal to currently-cached active interval, update active interval. */
-      if (!this.state.visualizer.activeIntervals[type].start || index !== this.state.visualizer.activeIntervals[type].index) {
-        setActiveInterval(state, { type, index })
-      }
-    })
-
-    /** Pass <canvas> 2d context, <canvas> width & height, and current track progress to background, then paint. */
-    this.state.visualizer.background.draw({
-      ctx: this.ctx,
-      width: this.canvas.width,
-      height: this.canvas.height,
-      trackProgress: this.state.visualizer.trackProgress
-    })
-    
-    /** Pass <canvas> 2d context and current track progress to each star, then paint. */
-    this.state.visualizer.stars.forEach(star => {
-      star.draw({
-        ctx: this.ctx,
-        trackProgress: this.state.visualizer.trackProgress
-      })
+    /** Paint elements based on track progress and active intervals. */
+    paint(this.state, {
+      canvas: this.canvas,
+      ctx: this.ctx
     })
   }
 }
