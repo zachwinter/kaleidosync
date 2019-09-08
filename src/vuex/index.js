dd5d46a384f61eee7011a4e295e3aa6a9e6d007e
Zach Winter <contact@zachwinter.com>
2019-09-07 21:10:51 -0700

    trails state bug fixes

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex'
 import VuexPersistence from 'vuex-persist'
 import Actions from './actions'
 import Mutations from './mutations'
-import { TRAILS } from '../enums'
 
 Vue.use(Vuex)
 
@@ -52,12 +51,58 @@ export default new Vuex.Store({
       autohide: true
     },
     visualizers: {
-      trails: { ...TRAILS.settings }
-    },
-    saved: [{
-      name: 'Default',
-      state: { ...TRAILS.settings }
-    }]
+      trails: {
+        SIDES: {
+          MIN: 3,
+          MAX: 24,
+          INITIAL: 6,
+          VALUE: 6,
+          STEP: 1
+        },
+        TRAIL_LENGTH: {
+          MIN: 7,
+          MAX: 25,
+          INITIAL: 20,
+          VALUE: 20,
+          STEP: 1
+        },
+        ROTATION_CONSTANT: {
+          MIN: 1,
+          MAX: 15,
+          INITIAL: 12,
+          VALUE: 12,
+          STEP: .25
+        },
+        ROTATION_MULTIPLIER: {
+          MIN: 1,
+          MAX: 1.2,
+          INITIAL: 1,
+          VALUE: 1,
+          STEP: .001
+        },
+        WIDTH_CONSTANT: {
+          MIN: .01,
+          MAX: .05,
+          INITIAL: .03,
+          VALUE: .03,
+          STEP: .001
+        },
+        GLOW_WIDTH: {
+          MIN: 20,
+          MAX: 100,
+          INITIAL: 40,
+          VALUE: 40,
+          STEP: 1
+        },
+        SMEAR: {
+          MIN: .1,
+          MAX: .9,
+          INITIAL: .8,
+          VALUE: .8,
+          STEP: .05
+        }
+      }
+    }
   },
   mutations: Mutations,
   actions: Actions,
