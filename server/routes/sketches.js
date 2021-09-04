12c61291c8755718245a74b0e33a24edf652f7b7
Zach Winter <contact@zachwinter.com>
2021-09-04 17:36:40 -0400

    6.2.1

--- server/routes/sketches.js
+++ server/routes/sketches.js
@@ -1,10 +1,23 @@
 const Sketch = require('../database/models/Sketch')
 
+let SKETCHES
+
+async function fetchSketches () {
+  try {
+    SKETCHES = await Sketch.find({ published: true })
+  } catch (e) {
+    console.log(e)
+  }  
+}
+
+setInterval(fetchSketches, 1000 * 60 * 5)
+
 module.exports = async app => {
+  await fetchSketches()
+
   app.get('/api/sketches/all-published', async (req, res) => {
     try {
-      const sketches = await Sketch.find({ published: true })
-      res.json({ success: true, sketches })
+      res.json({ success: true, sketches: SKETCHES })
     } catch (e) {
       console.log(e)
       res.json({ success: false, error: e })
