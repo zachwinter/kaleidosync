da61485fae35f84313b820eabe14630441824e79
Aric <spinningmonkey@gmail.com>
2022-04-29 00:09:58 -0700

    Removing Users permissions from Spotify API scope.

--- server/routes/authentication/login.js
+++ server/routes/authentication/login.js
@@ -5,7 +5,7 @@ module.exports = app => {
     const auth_id = Math.random().toString(36).slice(5, 11).toUpperCase()
     const query = queryString.stringify({
       response_type: 'code',
-      scope: ["playlist-read-collaborative playlist-read-private streaming user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state"],
+      scope: ["playlist-read-collaborative playlist-read-private streaming user-read-playback-state user-read-recently-played user-modify-playback-state"],
       state: auth_id,
       client_id: process.env.CLIENT_ID,
       redirect_uri: process.env.REDIRECT_URI,
