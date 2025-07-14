21983b2ed6a88b5965eb55c6967f5aec4bedf0b7
Zach Winter <contact@zachwinter.com>
2025-07-14 17:17:35 -0400

    10.0.20.

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import { createApp } from "@wearesage/static";
 createApp({
   port: process.env.PORT || 2223,
   directory: "dist",
-  apiUrl: process.env.API_URL || "http://localhost:2223"
+  apiUrl: process.env.VITE_API_BASE_URL || "http://localhost:2223"
 });
